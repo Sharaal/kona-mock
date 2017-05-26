b57211f59ea6a2153882f35918f258619fe306bd
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-26 14:05:31 +0200

    Send right reference #

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
 <H1>Service Unavailable</H1>
 The server is temporarily unable to service your request.  Please try again
 later.<P>
-Reference&#32;&#35;6&#46;43184a17&#46;1495793035&#46;75e2650
+Reference #12345.67890
 </BODY></HTML>
       `);
 };
